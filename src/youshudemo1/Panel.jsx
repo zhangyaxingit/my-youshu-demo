651775218259e9e2e46eb37adf14679188d72f68
zhangyaxingit <1052708321@qq.com>
2021-06-17 16:59:08 +0800

    多图标panel

--- src/youshudemo1/Panel.jsx
+++ src/youshudemo1/Panel.jsx
@@ -5,97 +5,37 @@ import * as echarts from 'echarts';
 import {data} from './mock'
 import {computeData} from './compute'
 
+const Container = memo(function Container({diagramDataArr, curDiagramId}) {
+    // console.log('创建新素222', diagramDataArr)
+    const [diagramNum, setDiagramNum] = useState(diagramDataArr.length)
 
-const Container = memo(function Container({chartConfig}) {
-    const main2 = useRef(null);
-    let chartInstance = null;
-    
     useEffect(() => {
-        renderChart();
-    })
-
-    let getChartOpt = (chartConfig) => {
-        const {
-            xSet, 
-            ySet, 
-            color,
-            selectChart,
-            selectCompute
-        } = chartConfig
-
-        let { finalData: totalData,  ruleArr} = computeData(data, {xSet, ySet, selectCompute}, color)
-
-        const xAxisData = Object.keys(totalData)
-        let yAxisData = null
-        let seriesData  = null
-        if (!ruleArr) {
-            yAxisData = xAxisData.map(x =>totalData[x])
-            seriesData = [
-                {
-                    data: yAxisData,
-                    type: selectChart
-                },
-            ]
-        } else {
-            yAxisData = ruleArr.map(rule => {
-                return {
-                    rule,
-                    value: xAxisData.map(x => totalData[x][rule])
-                }
-            })
-            seriesData = yAxisData.map(item => {
-                return {
-                    name: item.rule,
-                    type: selectChart,
-                    stack: 'rule',
-                    emphasis: {
-                        focus: 'series'
-                    },
-                    data: item.value
-                }
-            })
+        if(diagramNum) {
+            createNewDiagrm()
         }
+    }, [diagramNum])
 
+    useEffect(() => {
 
-        return {
-                tooltip: {
-                    trigger: 'axis'
-                },
-                toolbox: {
-                    feature: {
-                        saveAsImage: {}
-                    }
-                },
-                // legend: {
-                //     data: ['Remain']
-                // },
-                xAxis: {
-                    // boundaryGap: false,
-                    type: 'category',
-                    data: xAxisData
-                },
-                yAxis: {
-                    type: 'value'
-                },
-                series: seriesData
-            }
-    }
+        setDiagramNum(diagramDataArr.length)
+    }, [diagramDataArr])
 
-    let renderChart = () => {
-        const myChart = echarts.getInstanceByDom(main2.current);
-        if(myChart)
-            chartInstance = myChart;
-        else
-            chartInstance = echarts.init(main2.current);
+    let createNewDiagrm = () => {
+        
+        let newDiagram = document.createElement('div')
+        newDiagram.id = diagramDataArr.pop().id
+        newDiagram.style.width = '24rem'
+        newDiagram.style.height = '20rem'
+        newDiagram.style.backgroundColor = '#fff'
 
-        if (chartConfig) {
-            const chartOptions = getChartOpt(chartConfig)
-             chartInstance.setOption(chartOptions)
-        }
+        var panelNode = document.getElementById('panel');
+        panelNode.appendChild(newDiagram);
     };
+
     
-    return (<div id='panel' style={{ flex:1, height: '100%', backgroundColor:'#f8f8f8', display: 'flex', alignItems: 'center', justifyContent: 'center', paddingLeft: '4rem', paddingRight: '4rem' }}>
-        <div style={{width: '60rem', height: '50rem', backgroundColor: '#fff'}} id='chart' ref={main2}></div>
+    
+    return (<div id='panel' style={{ flex:1, height: '100%', backgroundColor:'#f8f8f8', display: 'flex', paddingLeft: '4rem', paddingRight: '4rem' }}>
+        {/* <div style={{width: '24rem', height: '20rem', backgroundColor: '#fff'}} id='chart' ref={main2}></div> */}
 	</div>);
 });
 
